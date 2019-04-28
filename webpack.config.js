05348f4099ec78cc0c17c6c3a2e49af4dce9aab9
Alex Ciarlillo <alex.ciarlillo@gmail.com>
2019-04-28 13:51:00 -0400

    bring in some player data

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,51 @@
-var path = require('path');
-var webpack = require('webpack');
+var path = require("path");
+var webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    devtool: 'eval',
-    mode: 'development',
-    entry: [
-        '@babel/polyfill',
-        './src/client/index'
-    ],
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/'
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: "./public/index.html",
-            favicon: "./public/favicon.ico"
-        })
-    ],
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                use: ['babel-loader'],
-                include: path.join(__dirname, 'src')
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-                loader: "url-loader?limit=100000"
-            }
-        ]
-    },
-    devServer: {
-        port: 3000,
-        open: true,
-        proxy: {
-            "/api": "http://localhost:8080"
-        }
-    },
+  devtool: "eval",
+  mode: "development",
+  entry: ["@babel/polyfill", "./src/client/index"],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/"
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      favicon: "./public/favicon.ico"
+    }),
+    new CopyWebpackPlugin(["public/players.json"])
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: ["babel-loader"],
+        include: path.join(__dirname, "src")
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: "url-loader?limit=100000"
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    proxy: {
+      "/api": "http://localhost:8080"
+    }
+  }
 };
