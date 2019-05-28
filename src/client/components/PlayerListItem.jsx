753f683ba721f8bf0099554d74337bba057c6453
Alex Ciarlillo <alex.ciarlillo@gmail.com>
2019-05-27 23:51:30 -0400

    more prettier

--- src/client/components/PlayerListItem.jsx
+++ src/client/components/PlayerListItem.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { observer } from "mobx-react";
+import React from 'react';
+import {observer} from 'mobx-react';
 
-const PlayerListItem = observer(({ player }) => (
+const PlayerListItem = observer(({player}) => (
   <li className="text-gray-700 bg-gray-200 p-4 text-xl">
     {player.name} - {player.team} - {player.position}
   </li>
